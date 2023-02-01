12a03d669f6f0768b70ede72ec5f32d1e8820b6b
Kaktus98 <juraj.rs@gmail.com>
2023-02-01 22:55:04 +0100

    pridany login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import "./Navbar"
-
+import React, { useState } from "react";
+import "./Navbar";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-        <div>
+    <form onSubmit={handleSubmit}>
+      <label for="email">email</label>
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        placeholder="email"
+        id="email"
+        name="email"
+      />{" "}
+      <br />
+      <label for="password">heslo</label>
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.password)}
+        type="heslo"
+        placeholder="heslo"
+        id="heslo"
+        name="heslo"
+      />{" "}
+      <br />
+      <button type="submit">Prihlásiť sa</button>
+    </form>
+  );
+};
 
-        </div>
-
-    
-  )
-}
-
-export default Login
+export default Login;
