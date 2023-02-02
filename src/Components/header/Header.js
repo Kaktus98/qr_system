3adc09548dcffd525f15ed05799da15dcc98d1e8
Kaktus98 <juraj.rs@gmail.com>
2023-02-02 20:53:44 +0100

    vytovrnenie hlavičky

---
+++ src/Components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <Header className="header">
+      <div className="logo">LOGO</div>
+      <div className="nazovStranky">Nazov Stránky</div>
+      {/* tu by mal ist navbar ale nie som si isty :D */}
+    </Header>
+  );
+};
+
+export default Header;
