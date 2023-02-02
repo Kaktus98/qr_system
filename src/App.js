99a4dda2776f3513b377c9a9659a97921f7d3550
Kaktus98 <juraj.rs@gmail.com>
2023-02-02 20:51:40 +0100

    tu som sa len pomylil :/

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+//rafce, skratka pre nacitanie predpisu funkcie
 import React from "react";
 import "./App.css";
 import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
